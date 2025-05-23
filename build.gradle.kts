e6ebd434f9ca7449792f41cbae97544675c4d757
Sofia Konovalova <sophia.konovalova2@gmail.com>
2025-05-23 20:08:40 +0200

    Revert "added all possible libraries"
    This reverts commit 20877af04ad5528ce40e4f1edcb582ec8ece7478.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,6 @@ dependencies {
     testImplementation("junit:junit:4.13.1")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:1.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
-    implementation("org.jctools:jctools-core:4.0.3")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
-    implementation("com.google.guava:guava:33.4.8-jre")
-    implementation("org.agrona:agrona:2.1.0")
 }
 
 tasks.test {
