f80b5cfb3cb0509f71841302629c601b2cf9cae2
Sofia Konovalova <sophia.konovalova2@gmail.com>
2025-05-23 11:31:06 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:lincheck:2.39")
+    testImplementation("junit:junit:4.13.1")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:1.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
