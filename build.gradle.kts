c0c2c5f02f5731fe7b1abecbc07c8f45943c331d
Sofia Konovalova <sophia.konovalova2@gmail.com>
2025-05-27 16:10:07 +0200

    added agrona and lmax libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:1.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
     implementation("org.jctools:jctools-core:4.0.3")
+    implementation("org.agrona:agrona:2.2.0")
+    implementation("com.lmax:disruptor:4.0.0")
 }
 
 tasks.test {
